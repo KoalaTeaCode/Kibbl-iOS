a5d3a620bb37c41a2d52c1efc80b3ac120ede4f4
Craig Holliday <hollidaycraig@yahoo.com>
2017-07-18 22:13:01 -0500

    rm pods

--- Pods/Target Support Files/Pods-Kibbl-IOS-Kibbl-IOSTests/Pods-Kibbl-IOS-Kibbl-IOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Kibbl_IOS_Kibbl_IOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_Kibbl_IOS_Kibbl_IOSTests
-@end
--- Pods/Target Support Files/Pods-Kibbl-IOS/Pods-Kibbl-IOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Kibbl_IOS : NSObject
-@end
-@implementation PodsDummy_Pods_Kibbl_IOS
-@end
